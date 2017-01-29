2dd962b01c92d5ab8a2431987398e8745af79fce
Wanyi Zhang <mushroomvv@gmail.com>
2017-01-29 12:33:02 -0500

    Use Mapbox
    Google Maps can’t display properly on GitHub Pages

---
+++ sub/js/leaflet.js
@@ -0,0 +1,8 @@
+var mymap = L.map('map').setView([37.428306, -173.135944], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'map',
+    accessToken: 'your.mapbox.public.access.token'
+}).addTo(mymap);
